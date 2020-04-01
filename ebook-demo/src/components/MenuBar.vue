607a10e49a7d0c0c8a77ccb59c24593058fa8aea
alan <jiejieshow@hotmail.com>
2020-04-01 12:45:16 +0800

    s

--- ebook-demo/src/components/MenuBar.vue
+++ ebook-demo/src/components/MenuBar.vue
@@ -1,5 +1,5 @@
 <template>
-     <transition name="slide-up">
+  <transition name="slide-up">
     <div class="menu-wrapper" v-show="ifTitleAndMenuShow">
       <div class="icon-wrapper">
         <span class="icon-3 icon"></span>
@@ -14,42 +14,40 @@
         <span class="icon-a icon">A</span>
       </div>
     </div>
-    </transition>
+  </transition>
 </template>
 <script>
-export default{
-    props: {
-       iftTitleAndMenuShow: {
-            type: Boolean,
-            default : false
-        }
+export default {
+  props: {
+    ifTitleAndMenuShow: {
+      type: Boolean,
+      default: false
     }
-}
-
+  }
+};
 </script>
 
 <style lang='scss' scoped>
-@import '@/src/assets/styles/global';
- .menu-wrapper {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    display: flex;
-    width: 100%;
-    height: px2rem(48);
-    z-index: 101;
-    background: white;
-    box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 15);
-    .icon-wrapper {
-      flex: 1;
-      @include center;
-    }
-    .icon-3 {
-      font-size: px2rem(18);
-    }
-    .icon-a {
-      font-size: px2rem(18);
-    }
-
+@import "@/src/assets/styles/global";
+.menu-wrapper {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  width: 100%;
+  height: px2rem(48);
+  z-index: 101;
+  background: white;
+  box-shadow: 0 px2rem(-8) px2rem(8) rgba(0, 0, 0, 15);
+  .icon-wrapper {
+    flex: 1;
+    @include center;
   }
+  .icon-3 {
+    font-size: px2rem(18);
+  }
+  .icon-a {
+    font-size: px2rem(18);
+  }
+}
 </style>
